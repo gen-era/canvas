20b2e4cedde97e700074217217107a5606e4e6ac
barissalman <baris.salman@gen-era.com.tr>
2024-07-18 16:26:40 +0300

    sample view eklendi

--- canvas/tables.py
+++ canvas/tables.py
@@ -1,27 +1,36 @@
 import django_tables2 as tables
+from django_tables2.utils import A  # alias for Accessor
+
 from canvas.models import Institution, Chip, Sample
 
 
 class InstitutionTable(tables.Table):
     class Meta:
         model = Institution
-        template_name = "canvas/bootstrap_htmx.html"
-        fields = ("name", )
-
+        template_name = "canvas/table_base.html"
+        fields = ("name",)
 
 
 class ChipTable(tables.Table):
     class Meta:
         model = Chip
-        template_name = "canvas/bootstrap_htmx.html"
-        fields = ("chip_id", "chip_type",)
+        template_name = "canvas/table_base.html"
+        fields = (
+            "chip_id",
+            "chip_type",
+        )
 
 
 class SampleTable(tables.Table):
-    chip = tables.Column(accessor = 'chipsample.first.chip.chip_id')
-    position = tables.Column(accessor = 'chipsample.first.position')
+
+    protocol_id = tables.TemplateColumn(
+        template_name="canvas/sample_protocol_id_column.html"
+    )
+
+    chip = tables.Column(accessor="chipsample.first.chip.chip_id")
+    position = tables.Column(accessor="chipsample.first.position")
 
     class Meta:
         model = Sample
-        template_name = "canvas/bootstrap_htmx.html"
+        template_name = "canvas/table_base.html"
         fields = ("protocol_id", "chip", "position", "sample_type", "institution")
--- canvas/urls.py
+++ canvas/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.Search.as_view(), name="search"),
+    path("sample/", views.SampleView.as_view(), name="sample"),
     path("chip_upload", views.ChipUpload.as_view(), name="chip_upload"),
 ]
