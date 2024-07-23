ed339e7ac807dc864fd499b4ab9dc707740d665e
sinan erdem <y.sinanerdem@gmail.com>
2024-07-23 18:26:58 +0300

    bedgraph vcf models

--- canvas/models.py
+++ canvas/models.py
@@ -1,4 +1,5 @@
 from django.core.validators import RegexValidator, FileExtensionValidator
+from django.core.exceptions import ValidationError
 
 from django.db import models
 from taggit.managers import TaggableManager
@@ -63,14 +64,6 @@ class Institution(models.Model):
         super().save(*args, **kwargs)
 
 
-def validate_position(value):
-    pattern = r"^R\d{2}C\d{2}$"  # Regex pattern for R01C01, R02C01, R03C01, etc.
-    if not bool(re.match(pattern, value)):
-        raise ValidationError(
-            "Position must follow the format R01C01, R02C01, R03C01, etc."
-        )
-
-
 class Sample(models.Model):
     entry_date = models.DateTimeField(
         auto_now_add=True
@@ -100,7 +93,13 @@ class ChipSample(models.Model):
 
     call_rate = models.DecimalField(max_digits=10, decimal_places=7)
     chip = models.ForeignKey(Chip, on_delete=models.PROTECT, null=True, blank=True, related_name="chipsample")
-    position = models.CharField(max_length=10, validators=[validate_position])
+    position = models.CharField(max_length=10, validators=[
+        RegexValidator(
+            regex=r"^R\d{2}C\d{2}$",
+            message='Position should follow this structure: R01C01',
+            code='invalid_position'
+        ),
+    ])
 
     def __str__(self):
         return f"{self.sample.protocol_id} - {self.chip.scan_date}"
@@ -153,7 +152,7 @@ class BedGraph(models.Model):
         auto_now_add=True
     )  # Change to DateTimeField with auto_now_add=True
     type = models.CharField(max_length=50,choices=bedgraph_types)
-    bedGraph = models.FileField(
+    bedgraph = models.FileField(
         upload_to="bedGraphs/",
         validators=[FileExtensionValidator(allowed_extensions=["bedgraph.gz"])],
     )
