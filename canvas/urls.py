ad4543aa778dd7146bb5b4be3ef248c5d028baa6
Sinan Erdem <y.sinanerdem@gmail.com>
2024-08-20 15:37:31 +0300

    Squashed commit of the following:
    commit 9b3f39e180c2eade4955a8865ae39f4edfe5cef3
    Author: Barış Salman <barslmn@gmail.com>
    Date:   Tue Aug 20 14:02:45 2024 +0300
    
        unutulmuş dosyalar
    
    commit c282b64e2cb8671513a496238ba5ec3e3443a013
    Author: Barış Salman <barslmn@gmail.com>
    Date:   Tue Aug 20 13:00:09 2024 +0300
    
        uini baştan

--- canvas/urls.py
+++ canvas/urls.py
@@ -4,7 +4,22 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("search/", views.Search.as_view(), name="search"),
-    path("sample/", views.SampleView.as_view(), name="sample"),
+    path("sample_search", views.sample_search, name="sample_search"),
+    path(
+        "institution_search/",
+        views.institution_search,
+        name="institution_search",
+    ),
+    path("chip_search", views.chip_search, name="chip_search"),
+    path(
+        "chipsample_tab_content",
+        views.chipsample_tab_content,
+        name="chipsample_tab_content",
+    ),
+    path(
+        "chipsample_tab_button",
+        views.chipsample_tab_button,
+        name="chipsample_tab_button",
+    ),
     path("chip_upload", views.ChipUpload.as_view(), name="chip_upload"),
 ]
